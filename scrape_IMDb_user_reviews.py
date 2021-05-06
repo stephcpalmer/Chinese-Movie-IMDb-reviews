90b104c14411c320069fff3612e32ca15bcdb294
stephcpalmer <83552472+stephcpalmer@users.noreply.github.com>
2021-05-06 16:46:13 -0400

    Add files via upload
    Code to create dataframe for imdb ratings data collected for each review

--- scrape_IMDb_user_reviews.py
+++ scrape_IMDb_user_reviews.py
@@ -1,64 +1,64 @@
-from reppy.robots import Robots
 import urllib.request
 from bs4 import BeautifulSoup
 import pandas as pd
 import pprint, re
 
-#first check to see if ok to scrape user ratings data   ##From reppy github documentation ##tt0013807
 Url = 'https://www.imdb.com/title/tt*/reviews'
-robots = Robots.fetch('https://www.imdb.com/robots.txt')
 
 Base_url = 'https://www.imdb.com/title/'
 End_url = '/reviews/'
 
 
-if robots.allowed(Url, 'SCP') is False: #Making sure that I am allowed to scrape from the movie title's page   #I can so this won't happen, but just in case
-    print(f"Cannot scrape from {Url}.")   
+data = pd.read_csv('Dataframe.txt', delimiter = ",")
+data = data.drop(columns = 'Unnamed: 0')
+    
+User_reviews_dict = {}
 
-else:
-    delay = robots.agent('SCP').delay   #Checking to see if there is a crawl delay specified   #There is none 
-    data = pd.read_csv('Dataframe.txt', delimiter = ",")
-    data = data.drop(columns = 'Unnamed: 0')
-    
-    User_reviews_dict = {}
+Col_names = ['Id','User','Rating','Date Posted']
+User_ratings_df = pd.DataFrame()
 
-    Col_names = ['Id','User','Rating','Date Posted']
-    User_ratings_df = pd.DataFrame(columns=Col_names)
-    
-    
-    for i in range(10):#len(data.index)
+
+for i in range(len(data.index)):
+    try:
+        with urllib.request.urlopen(Base_url+data.at[i,'Id']+End_url,timeout=10) as request:
+                soup = BeautifulSoup(request.read(),'lxml')
         try:
-            with urllib.request.urlopen(Base_url+data.at[i,'Id']+End_url,timeout=10) as request:
-                    soup = BeautifulSoup(request.read(),'lxml')
-            try:
-                User_reviews = soup.find_all('div', {'class': 'text show-more__control'})   # a list of user reviews # removed limit in hopes to improve topic models # call User_reviews[i].contents for individual reviews
-                User_reviews_dict[data.at[i,'Id']] = {} #creating nested dictionary {'id1':{0:'first review',1:'second review},'id2':{0:'first review of 2nd id',1:'second review of 2nd id}}...
-                for j in range(1,1+len(User_reviews)):
-                    User_reviews_dict[data.at[i,'Id']][j] = User_reviews[j].text
+            User_reviews = soup.find_all('div', {'class': 'text show-more__control'})   # a list of user reviews # removed limit in hopes to improve topic models # call User_reviews[i].contents for individual reviews
+            User_reviews_dict[data.at[i,'Id']] = {} #creating nested dictionary {'id1':{0:'first review',1:'second review},'id2':{0:'first review of 2nd id',1:'second review of 2nd id}}...
+            
+            for j in range(len(User_reviews)):
+                User_reviews_dict[data.at[i,'Id']][j] = User_reviews[j].text
+                
+            Ratings = []
 
-                    User_ratings = soup.find_all('div',{'class':'ipl-ratings-bar'})
-                    User_names_and_dates = soup.find_all('div',{'class':'display-name-date'})
-                    for k in range(1,1+len(User_ratings)):
-                        User_rating_num = data.at[i,'Id']+'_'+str(k)
-                        User_ratings_df.at[i+j+k,'Id'] = User_rating_num
-                            
-                        User = User_names_and_dates[k].contents[1].text
-                        User_ratings_df.at[i+j+k,'User'] = User
-                            
-                        Rating = re.sub('\n','',User_ratings[k].text)
-                        User_ratings_df.at[i+j+k,'Rating'] = Rating
-                            
-                        Date_posted = User_names_and_dates[k].contents[2].text
-                        User_ratings_df.at[i+j+k,'Date Posted'] = Date_posted
-            except:
-                data.drop(i)
-        except: 
-            print(f"Request for {i}'s user reviews timed out")
-
+            for k in range(len(User_reviews)):
+                
+                user_rating_num = data.at[i,'Id']+'_'+str(k)
+            
+                if len(User_reviews[k].parent.parent.contents)<=7:
+                    rating = 'N/A'
+                    user = User_reviews[k].parent.parent.contents[3].contents[1].text
+                    date_posted = User_reviews[2].parent.parent.contents[3].contents[2].text
+                    
+                elif len(User_reviews[k].parent.parent.contents)>=9:
+                    rating = re.sub('\n','',User_reviews[k].parent.parent.contents[1].text)
+                    user = User_reviews[k].parent.parent.contents[5].contents[1].text
+                    date_posted = User_reviews[k].parent.parent.contents[5].contents[2].text
+                    
+                Ratings.append([user_rating_num,user,rating,date_posted])
+                
+            User_ratings_df = User_ratings_df.append(Ratings,ignore_index=True)
+                
+        except:
+            data.drop(i)
         print(i)
+    except: 
+        print(f"Request for {i}'s user reviews timed out")
+
 User_ratings_df.to_csv('User_ratings_dataframe.txt')
+
 with open('User_reviews.txt', 'w',encoding='utf8') as wf:
-    wf.write(pprint.pformat(User_reviews_dict, depth=2))
+    wf.write(pprint.pformat(User_reviews_dict, depth=0))
 
     
 
